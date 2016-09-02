4de913f010ebfb87b783dc29076bf2239fd56ecf
Kelly Kinkade <kelly.lynn.martin@gmail.com>
2016-09-02 14:15:43 -0500

    Initial commit of dinky

---
+++ README.md
@@ -0,0 +1,6 @@
+Dinky
+=====
+
+This is a toy mod.
+
+Current Forge target: Minecraft 1.10.2, Forge 12.18.1.2066
---
+++ src/org/pyrzqxgl/dinky/Dinky.scala
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) Kelly Kinkade (ab9rf) 2016
+ * https://github.com/ab9rf/Dinky
+ *
+ * This mod is distributed under the terms of the Minecraft Mod Public
+ * License 1.0, or MMPL. Please check the contents of the license located in
+ * http://....
+ */
+
+package org.pyrzqxgl.dinky
+
+import net.minecraftforge.fml.common.Mod
+
+@Mod(modid = Dinky.modId, version = "Dinky_VER", name = "DinkyMod", modLanguage = "scala")
+object Dinky {
+  var instance = this
+
+  final val modId = "Dinky"
+
+}
+
